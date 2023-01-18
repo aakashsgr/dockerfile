1f1534148118f2d7d75fe9ef69e1de8dc067d918
aakashsgr <aakash.sagar.vs@gmail.com>
2023-01-18 10:24:34 +0530

    Update Task1_Test.py

--- test/Task1_Test.py
+++ test/Task1_Test.py
@@ -11,7 +11,7 @@ import Product as product
 class Testing(unittest.TestCase):
     
     def test_addProduct(self):
-        self.assertEqual(product.Product.addProduct(5), 5)
+        self.assertEqual(product.Product.addProduct(5), 6)
 
     def test_removeProduct(self):
         self.assertEqual(product.Product.removeProduct(5), 4)
